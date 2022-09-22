41c5121adb2c30f0c2ffd289fb516be401539a73
Nitish Kumar Bansal <nitishbansal@Nitishs-MacBook-Pro.local>
2022-09-22 23:26:27 +0530

    Speaker image add

--- frontend/src/Components/Speakers/Speakers.js
+++ frontend/src/Components/Speakers/Speakers.js
@@ -1,9 +1,10 @@
 import styles from "./Speakers.module.css";
 import Banner from "../../Images/SpeakersBanner.png";
-import SampleImage1 from "../../Images/SampleImage1.png";
-import SampleImage2 from "../../Images/SampleImage2.png";
-import SampleImage3 from "../../Images/SampleImage3.png";
+import SampleImage1 from "../../Images/Speaker1.png";
+import SampleImage2 from "../../Images/Speaker2.png";
+import SampleImage3 from "../../Images/Speaker3.png";
 import SampleImage4 from "../../Images/SampleImage4.png";
+import Linkedin from "../../Images/Linkedin.png";
 
 import { useState } from "react";
 
@@ -21,6 +22,7 @@ const Speakers = () => {
 
             <div className={styles.banner_bg}></div>
             <div className={styles.container}>
+                <h1>Speakers</h1>
                 <div className={styles.container_row_1}>
                     <div className={styles.box_main_container}>
                         <div className={styles.box_main_top}>
@@ -30,13 +32,14 @@ const Speakers = () => {
                                 className={styles.box_main_yellow_button}
                             ></div>
                         </div>
+
                         <h1>{title}</h1>
                         <p>{desc}</p>
                     </div>
                     <div
                         className={styles.box}
                         onClick={() => (
-                            setTitle("Queen Elizabeth"),
+                            setTitle("Akhil Sharma"),
                             setDesc(
                                 "dead;f my mom went to get me food but she has been gone for three hours. If she's not home we will be stuck with only frozen pizza and French fries. She's making me really upset."
                             )
@@ -47,7 +50,7 @@ const Speakers = () => {
                     <div
                         className={styles.box}
                         onClick={() => (
-                            setTitle("Charles King"),
+                            setTitle("Pravin Hungund"),
                             setDesc(
                                 "Boht achha padhta hai, editorial give some content :("
                             )
@@ -60,13 +63,24 @@ const Speakers = () => {
                     <div
                         className={styles.box}
                         onClick={() => (
-                            setTitle("Abhishek Agarwal"),
+                            setTitle("Gautam Sampathkumar"),
                             setDesc(
                                 "Boht achha padhta hai, editorial give some content :("
                             )
                         )}
                     >
                         <img src={SampleImage3} alt="" />
+                    </div>
+                    <div
+                        className={styles.box}
+                        onClick={() => (
+                            setTitle("African Prince Dhaval"),
+                            setDesc(
+                                "Boht achha padhta hai, editorial give some content :("
+                            )
+                        )}
+                    >
+                        <img src={SampleImage4} alt="" />
                     </div>
                     <div
                         className={styles.box}
