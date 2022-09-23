29dd91b7c9a66a5c8126517c9593eaa2e8fff99f
Nitish Kumar Bansal <nitishbansal@Nitishs-MacBook-Pro.local>
2022-09-23 20:39:26 +0530

    Speaker Update

--- frontend/src/Components/Speakers/Speakers.js
+++ frontend/src/Components/Speakers/Speakers.js
@@ -5,6 +5,7 @@ import SampleImage2 from "../../Images/Speaker2.png";
 import SampleImage3 from "../../Images/Speaker3.png";
 import SampleImage4 from "../../Images/Speaker4.png";
 import Linkedin from "../../Images/Linkedin.png";
+import Link from "../../Images/Link.svg";
 
 import { useState } from "react";
 
@@ -38,13 +39,18 @@ const Speakers = () => {
                                 className={styles.box_main_yellow_button}
                             ></div>
                         </div>
-                        <a href={url} target="_blank">
+                        <div className={styles.title_link_image}>
                             <h1>{title}</h1>
-                        </a>
+                            <a href={url} target="_blank">
+                                <img src={Link} />
+                            </a>
+                        </div>
+
                         <p>{desc}</p>
                     </div>
                     <div
                         className={styles.box}
+                        style={{ boxShadow: "8px 8px 0px #675943" }}
                         onClick={() => (
                             setTitle("Akhil Sharma"),
                             setDesc(
@@ -57,10 +63,11 @@ const Speakers = () => {
                     </div>
                     <div
                         className={styles.box}
+                        style={{ boxShadow: "8px 8px 0px #023671" }}
                         onClick={() => (
                             setTitle("Pravin Hungund"),
                             setDesc(
-                                "Boht achha padhta hai, editorial give some content :("
+                                "He is widely experienced in leading H2, H3 strategic development programs across Cloud, AI, Metaverse -AR/VR/MR, IOT, Robotics & Drones, 5G, Blockchain.He has designed and delivered 100+ fully immersive solutions as a Solutions Strategist."
                             ),
                             setUrl("https://www.linkedin.com/in/pravinhungund")
                         )}
@@ -71,6 +78,7 @@ const Speakers = () => {
                 <div className={styles.container_row_2}>
                     <div
                         className={styles.box}
+                        style={{ boxShadow: "8px 8px 0px #632301" }}
                         onClick={() => (
                             setTitle("Gautam Sampathkumar"),
                             setDesc(
@@ -83,12 +91,13 @@ const Speakers = () => {
                     </div>
                     <div
                         className={styles.box}
+                        style={{ boxShadow: "8px 8px 0px rgb(87, 2, 116)" }}
                         onClick={() => (
                             setTitle("Ami Sampath"),
                             setDesc(
-                                "Boht achha padhta hai, editorial give some content :("
+                                "She is currently a senior UX designer at Amazon, India and has over 8 years of experience working in the design domain in various reputable companies such as SAP, Bosch etc. She has completed her bachelor's from VIT and her master's in product design from IISC Bangalore."
                             ),
-                            setUrl("")
+                            setUrl("https://www.linkedin.com/in/ami-sampat")
                         )}
                     >
                         <img src={SampleImage4} alt="" />
